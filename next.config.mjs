5f94023f28ec4d484e269d92048ebe5e4de2af45
Vinay Vengala <vinay.vengala@kore.com>
2024-02-11 23:18:55 +0530

    Initial

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 import BuilderDevTools from "@builder.io/dev-tools/next";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = BuilderDevTools()({});
+const nextConfig = BuilderDevTools()({
+    output: 'export',
+});
 
 export default nextConfig;
