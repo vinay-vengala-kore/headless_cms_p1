4be3e5bbaed52121861c529e0c7dbb55f7a69907
vinay-vengala-kore <159417219+vinay-vengala-kore@users.noreply.github.com>
2024-02-23 05:24:09 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,1 @@
-/** import BuilderDevTools from "@builder.io/dev-tools/next";
-
-/* * @type {import('next').NextConfig} * /
-const nextConfig = BuilderDevTools()({
-    output: 'export',
-});
-
-export default nextConfig;*/
-
-
 /** @type {import('next').NextConfig} */ const nextConfig = { output: "export", // <=== enables static exports reactStrictMode: true, }; module.exports = nextConfig;
